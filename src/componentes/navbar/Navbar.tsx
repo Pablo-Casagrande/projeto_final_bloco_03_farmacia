332906c7c974fadbba8419f8be6b86aeb0a83f24
unknown <furtadocasagrandepablo@gmail.com>
2025-07-02 10:08:00 -0300

    :sparkles: Criando os componentes Home, Navbar e Footer

---
+++ src/componentes/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    Farmacia
+
+                    <div className='flex gap-4'>
+                        Categoria
+                        Cadastrar categoria
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+
+function Home() {
+    return (
+        <>
+            <div className="bg-indigo-200 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vindo!
+                        </h2>
+                        <p className='text-xl'>
+                            Aqui voce encontra Medicamentos e Cosmeticos!
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Cadastrar produto
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://media.discordapp.net/attachments/1356053907838730460/1389941380595318874/home.png?ex=68667348&is=686521c8&hm=bb3a087c657cc552a96e51b8512996df6779940e43ca3e85b1e8b2565ee9d3a2&=&format=webp&quality=lossless&width=759&height=763"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
